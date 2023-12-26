ea54e0a9f4ccdddd1e21e7e230e716e49e2b739c
Andriy0 <andriy2152@gmail.com>
2023-12-26 14:01:37 +0200

    Add lab 8

---
+++ lab8.py
@@ -0,0 +1,20 @@
+import openpyxl as opl
+from scipy.stats import pearsonr
+
+ws = opl.load_workbook('./PV2013June.xlsx')['07-06-2013']
+
+def read_column(ws, col):
+  return [ws.cell(row=i, column=col).value for i in range(5, ws.max_row + 1)]
+
+def print_stats(dataArray1, dataArray2, title1, title2):
+  print(title1, 'and', title2)
+  print(pearsonr(dataArray1, dataArray2))
+  print('\n')
+
+data = [[read_column(ws, 6), 'Temperature'],
+        [read_column(ws, 9), 'Frequency'],
+        [read_column(ws, 16), 'Generated Power']]
+
+print_stats(data[0][0], data[1][0], data[0][1], data[1][1])
+print_stats(data[1][0], data[2][0], data[1][1], data[2][1])
+print_stats(data[2][0], data[0][0], data[2][1], data[0][1])
